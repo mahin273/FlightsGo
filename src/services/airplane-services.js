a2ce9a65d08317ec4d7517749366d1cbe0bf33d9
Mahin Khan <md.mahin.bd18@gmail.com>
2024-08-24 00:34:31 +0600

    added some validation error handling task

--- src/services/airplane-services.js
+++ src/services/airplane-services.js
@@ -1,1 +1,28 @@
-throw new AppError('Cannot create a new Airplane object', StatusCodes.INTERNAL_SERVER_ERROR);
+const { StatusCodes } = require('http-status-codes');
+const { AirplaneRespository } = require('../repositories');
+const AppError = require('../utils/errors/app-error');
+
+const airplaneRespository = new AirplaneRespository();
+
+async function createAirplane(data) {
+    try {
+        const airplane = await airplaneRespository.create(data);
+        return airplane;
+        
+    } catch (error) {
+        if (error.name == 'SequelizeValidationError') {
+            let explanation = [];
+            error.errors.forEach((err) => {
+                explanation.push(err.message);
+            });
+            
+            throw new AppError(explanation,StatusCodes.BAD_REQUEST);
+        }
+
+        throw new AppError('Cannot create a new Airplane object', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+module.exports = {
+    createAirplane
+}
