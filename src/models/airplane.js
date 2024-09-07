ae1e3a409d1d32b8723ca3abda9f0e8102a9ffeb
Mahin Khan <md.mahin.bd18@gmail.com>
2024-09-08 03:03:45 +0600

    Added seats model in db associated to airplane

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -16,6 +16,10 @@ module.exports = (sequelize, DataTypes) => {
         as: 'flights',
         onDelete: 'CASCADE'
       });
+      this.belongsTo(models.Seat, {
+        foreignKey: 'airplaneId',
+        as:'airplaneDetail'
+      });
     }
   }
   Airplane.init({
